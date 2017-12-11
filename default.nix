adac7b19bbca158763cdf062b2d74d43cb5cc344
Dylan Simon <dylan@dylex.net>
2017-12-10 21:55:49 -0500

    Add gcc7 build; switch to native tools
    requires nixpkgs PR#32495 631f5a167c44677816ffa617c2c75d11d9be7775

--- default.nix
+++ default.nix
@@ -1,14 +1,24 @@
-let nixpkgs = import <nixpkgs> {
-  #stdenvStages = import <nixpkgs/pkgs/stdenv/native>;
-}; in
+with import <nixpkgs> {
+  stdenvStages = import <nixpkgs/pkgs/stdenv/native>;
+};
 
 rec {
-  gmp = nixpkgs.callPackage ./base/gmp { };
-  mpfr = nixpkgs.callPackage ./base/mpfr {
+  gmp = callPackage base/gmp {
+  };
+  mpfr = callPackage base/mpfr {
     inherit gmp;
   };
-  mpc = nixpkgs.callPackage ./base/mpc {
+  mpc = callPackage base/mpc {
     inherit gmp;
     inherit mpfr;
+  };
+  gcc7 = wrapCC (callPackage devel/gcc/7.2.0.nix {
+    inherit gmp;
+    inherit mpfr;
+    inherit mpc;
+  });
+
+  gcc7env = stdenv.override {
+    cc = gcc7;
   };
 }
