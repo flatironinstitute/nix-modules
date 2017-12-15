86d020bc433afc0a49f5ff5efc8bdebb595f0647
Dylan Simon <dylan@dylex.net>
2017-12-14 19:37:22 -0500

    Use standard nixpkgs for gcc7 build

--- default.nix
+++ default.nix
@@ -1,6 +1,19 @@
-with import <nixpkgs> {
-  stdenvStages = import <nixpkgs/pkgs/stdenv/native>;
-};
+let
+  nativeTools = false;
+  lib = import <nixpkgs/lib>;
+  nativePlatform = (import <nixpkgs/lib/systems/platforms.nix> { inherit lib; }).selectBySystem builtins.currentSystem // {
+    gcc = { arch = "native"; };
+  };
+  config = {
+    platform = nativePlatform;
+  };
+  pkgs = import <nixpkgs> {
+    inherit config;
+    stdenvStages = import (if nativeTools then <nixpkgs/pkgs/stdenv/native> else <nixpkgs/pkgs/stdenv>);
+  };
+in
+
+with pkgs;
 
 rec {
   gmp = callPackage base/gmp {
@@ -12,13 +25,23 @@ rec {
     inherit gmp;
     inherit mpfr;
   };
-  gcc7 = wrapCC (callPackage devel/gcc/7.2.0.nix {
-    inherit gmp;
-    inherit mpfr;
-    inherit mpc;
-  });
+  gcc7 = wrapCC (if nativeTools
+    then callPackage devel/gcc/7.2.0.nix {
+      inherit gmp;
+      inherit mpfr;
+      inherit mpc;
+    }
+    else pkgs.gcc7.cc.override {
+      langC = true;
+      langCC = true;
+      langFortran = true;
+      langObjC = true;
+      langObjCpp = true;
+      enableShared = true;
+      enableMultilib = false;
+    });
 
-  gcc7env = stdenv.override {
-    cc = gcc7;
+  gcc7env = overrideCC stdenv gcc7;
+
   };
 }
