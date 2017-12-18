e32778ff96850689ecd2bb62ebd58dfafdd42df6
Dylan Simon <dylan@dylex.net>
2017-12-18 15:23:18 -0500

    Reorganize, defining local callPackage
    Still can't get an overlay to work properly, though this claims it's
    possible:
    http://nix-dev.science.uu.narkive.com/xPt3pOg0/replace-default-gcc-through-overlays

--- default.nix
+++ default.nix
@@ -1,89 +1,87 @@
 let
+  # Try to use native tools to bootstrap (broken):
   nativeTools = false;
+
   lib = import <nixpkgs/lib>;
-  nativePlatform = (import <nixpkgs/lib/systems/platforms.nix> { inherit lib; }).selectBySystem
-    builtins.currentSystem // {
-      gcc = { arch = "native"; };
-    };
-  config = {
-    platform = nativePlatform;
-  };
-  overlays = [];
   pkgs = import <nixpkgs> {
-    inherit config overlays;
+    # Use current platform, setting gcc.arch = "native":
+    localSystem = rec {
+      system = builtins.currentSystem;
+      platform = lib.systems.platforms.selectBySystem system // { gcc = { arch = "native"; }; };
+    };
+    overlays = [];
     stdenvStages = import (if nativeTools then <nixpkgs/pkgs/stdenv/native> else <nixpkgs/pkgs/stdenv>);
   };
-in
 
-with pkgs;
+  local = self: rec {
+    # gcc bootstrap packages for native only
+    gmp = pkgs.callPackage base/gmp { };
+    mpfr = pkgs.callPackage base/mpfr {
+      inherit gmp;
+    };
+    mpc = pkgs.callPackage base/mpc {
+      inherit gmp mpfr;
+    };
 
-pkgs // rec {
-  gmp = callPackage base/gmp {
+    gcc6 = pkgs.wrapCC (if nativeTools
+      then pkgs.callPackage devel/gcc/6.4.0.nix {
+	inherit gmp mpfr mpc;
+      }
+      else pkgs.gcc6.cc.override {
+	langC = true;
+	langCC = true;
+	langFortran = true;
+	langObjC = true;
+	langObjCpp = true;
+	enableShared = true;
+	enableMultilib = false;
+      });
+
+    gcc7 = pkgs.wrapCC (if nativeTools
+      then pkgs.callPackage devel/gcc/7.2.0.nix {
+	inherit gmp mpfr mpc;
+      }
+      else pkgs.gcc7.cc.override {
+	langC = true;
+	langCC = true;
+	langFortran = true;
+	langObjC = true;
+	langObjCpp = true;
+	enableShared = true;
+	enableMultilib = false;
+      });
+
+    # define new local environment:
+    gcc = gcc6;
+    gfortran = gcc;
+    stdenv = pkgs.overrideCC pkgs.stdenv gcc;
+    callPackage = pkgs.newScope self;
+
+    # intel infiniband/psm stuff
+    infinipath-psm = callPackage base/infinipath-psm { };
+    libpsm2 = callPackage base/libpsm2 { };
+    rdma-core = callPackage base/rdma-core { };
+
+    openmpi = callPackage devel/openmpi/1.10.7.nix { };
+    openmpi2 = callPackage devel/openmpi/2.1.2.nix { };
+
+    fftw = callPackage base/fftw/precs.nix { };
+    fftw-openmpi1 = callPackage base/fftw/precs.nix {
+      mpi = openmpi;
+    };
+    fftw-openmpi2 = callPackage base/fftw/precs.nix {
+      mpi = openmpi2;
+    };
+
+    openblas = callPackage base/openblas {
+    };
+
+    hdf5 = pkgs.hdf5.override {
+      cpp = true;
+    };
+
+    python2 = pkgs.python2.override {
+      ucsEncoding = 4;
+    };
   };
-  mpfr = callPackage base/mpfr {
-    inherit gmp;
-  };
-  mpc = callPackage base/mpc {
-    inherit gmp mpfr;
-  };
-
-  gcc7 = wrapCC (if nativeTools
-    then callPackage devel/gcc/7.2.0.nix {
-      inherit gmp mpfr mpc;
-    }
-    else pkgs.gcc7.cc.override {
-      langC = true;
-      langCC = true;
-      langFortran = true;
-      langObjC = true;
-      langObjCpp = true;
-      enableShared = true;
-      enableMultilib = false;
-    });
-
-  stdenv = overrideCC pkgs.stdenv gcc7;
-
-  infinipath-psm = callPackage base/infinipath-psm {
-    inherit stdenv;
-  };
-
-  libpsm2 = callPackage base/libpsm2 {
-    inherit stdenv;
-  };
-
-  rdma-core = callPackage base/rdma-core {
-    inherit stdenv;
-  };
-
-  openmpi = callPackage devel/openmpi/1.10.7.nix {
-    inherit stdenv infinipath-psm libpsm2 rdma-core;
-  };
-
-  openmpi2 = callPackage devel/openmpi/2.1.2.nix {
-    inherit stdenv infinipath-psm libpsm2 rdma-core;
-  };
-
-  fftw = callPackage base/fftw/precs.nix {
-    inherit stdenv;
-  };
-
-  fftw-openmpi1 = callPackage base/fftw/precs.nix {
-    inherit stdenv;
-    mpi = openmpi;
-  };
-
-  fftw-openmpi2 = callPackage base/fftw/precs.nix {
-    inherit stdenv;
-    mpi = openmpi2;
-  };
-
-  openblas = callPackage base/openblas {
-    inherit stdenv;
-  };
-
-  hdf5 = pkgs.hdf5.override {
-    inherit stdenv;
-    cpp = true;
-    gfortran = stdenv.cc;
-  };
-}
+in pkgs // lib.fix local
